d696a5cba56182d7297ca3341a16630887fc558e
BPL08 <locdm123@gmail.com>
2025-11-27 13:04:16 +0700

    fix

--- src/EMRS.Infrastructure/Persistence/EMRSDbContext.cs
+++ src/EMRS.Infrastructure/Persistence/EMRSDbContext.cs
@@ -25,7 +25,7 @@ namespace EMRS.Infrastructure.Persistence;
     public DbSet<HolidayPricing> HolidayPricings { get; set; }
     public DbSet<InsuranceClaim> InsuranceClaims { get; set; }
     public DbSet<InsurancePackage> InsurancePackages { get; set; }
-  
+    public DbSet<GPSSharing> GPSSharings { get; set; }
     public DbSet<Media> Media { get; set; }
     public DbSet<Membership> Memberships { get; set; }
     public DbSet<RentalContract> RentalContracts { get; set; }
