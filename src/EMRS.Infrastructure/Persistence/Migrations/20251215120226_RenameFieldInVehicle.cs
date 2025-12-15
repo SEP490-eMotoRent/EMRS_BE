461e73563f24b14cb242345b8455681228e08fd7
BPL08 <locdm123@gmail.com>
2025-12-15 19:04:18 +0700

    add Migrations

---
+++ src/EMRS.Infrastructure/Persistence/Migrations/20251215120226_RenameFieldInVehicle.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMRS.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameFieldInVehicle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "year_of_manufacture",
+                table: "vehicles",
+                newName: "date_manufacturing");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "date_manufacturing",
+                table: "vehicles",
+                newName: "year_of_manufacture");
+        }
+    }
+}
